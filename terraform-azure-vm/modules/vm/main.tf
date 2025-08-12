ea8402120e94d1c72161faaec684665828a6dd0c
Ubuntu <azureuser@Devops-intern.zfwmpm5optmuvgdr10eui0rc4a.hx.internal.cloudapp.net>
2025-08-12 08:08:56 +0000

    add network security group

--- terraform-azure-vm/modules/vm/main.tf
+++ terraform-azure-vm/modules/vm/main.tf
@@ -17,6 +17,29 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}-${var.environment}-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_public_ip" "pip" {
   name                = "${var.prefix}-${var.environment}-pip"
   location            = var.location
